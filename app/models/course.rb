3abc5ac5063d7811b0af659b3d9d55314ab3a85d
bisquickman231 <whuskey6@gmail.ocm>
2018-10-07 20:53:29 -0600

    Added validations to all of the models.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
+  validates :name, presence: true
+  validates :department, presence: true
+  validates :number, presence: true
+  validates :creditHours, presence: true
   has_and_belongs_to_many :sections
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,6 @@
 class Section < ApplicationRecord
+  validates :semester, presence: true
+  validates :number, presence: true
+  validates :roomNumber, presence: true
   has_and_belongs_to_many :courses
 end
