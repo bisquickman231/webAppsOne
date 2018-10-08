1504443648c7736515edb9aebca1df817a8a6bfc
bisquickman231 <whuskey6@gmail.ocm>
2018-10-07 19:05:09 -0600

    Edited the joint table. made sure both has and belongs to many

--- app/models/sections_student.rb
+++ app/models/sections_student.rb
@@ -1,4 +1,4 @@
-class SectionsStudent < ApplicationRecord
-  belongs_to :section
+class SectionsStudents < ApplicationRecord
+  has_and_belongs_to_many :section
   has_and_belongs_to_many :students
 end
